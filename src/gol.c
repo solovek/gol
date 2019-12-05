ddae5ce9b998833de1574dc9e68418f95ea8f599
GabrielBittencourt <gdsbittencourt@inf.ufpel.edu.br>
2019-12-05 09:31:53 -0500

    regras

--- src/gol.c
+++ src/gol.c
@@ -7,6 +7,21 @@ void gol (void)
   
 }
 /*
+
+for(int i = 0; i < 150; ++i){
+  for(int j = 0; j < 200; ++j){
+    regras(cur_state, i, j);
+  }
+}
+ regras(cur_state, y, x){
+  acumulador = 0;
+  for(int i = -1; i <= 1; ++i){
+   for(int j = -1; j <= 1; ++j){
+     if(cur_state[y+i][x+j])
+     ++acumulador; 
+   }
+ }
+}
     Any live cell with fewer than two live neighbours dies, as if by underpopulation.
     Any live cell with two or three live neighbours lives on to the next generation.
     Any live cell with more than three live neighbours dies, as if by overpopulation.
